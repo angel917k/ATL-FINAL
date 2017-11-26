8e3530a1c1462f042c225c201151876043f9fb24
J.R. Ruggiero <fleury14@comcast.net>
2017-11-26 09:54:50 -0800

    edited form html to not submit if the form isnt valid. also edited ts to make sure submits dont go through if theyre not logged in. successfully tested tip submission

--- src/app/tip-page/tip-page.component.ts
+++ src/app/tip-page/tip-page.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+
+import { TipService } from './../services/tip.service';
+import { LoginService } from './../services/login.service';
 
 @Component({
   selector: 'app-tip-page',
@@ -7,9 +12,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TipPageComponent implements OnInit {
 
-  constructor() { }
+  private _tipForm: NgForm; // holds the form object
+  public content: string; // hold the variable that is two-way binded to the textarea
+  public loggedInUser; // will grab login info from the login service
+
+  constructor(private _tipService: TipService, private _loginService: LoginService) { }
 
   ngOnInit() {
-  }
+    // get login info as soon as page is loaded
+    this._loginService.getLoggedInUser()
+    .subscribe( user => {
+        this.loggedInUser = user;
+    });
+  } // end oninit
+
+    // submits a tip into the array 'tips' of the document that matches up with the current logged in users id
+  public submitTipIntoArray() {
+
+    // immediately stops the function if someone submits without logging in.
+    if (!this.loggedInUser) {
+      return;
+    }
+
+    // create an object that will conform to the ITip interface we set earlier.
+    const newTip = {
+        date: new Date(),
+        content: this.content
+    };
+
+    // console.log('Submitting!!', newTip);
+    this._tipService.pushTip(newTip, this.loggedInUser.uid)
+    .subscribe( data => {
+        console.log( 'We good?');
+        this.content = ''; // reset textarea after submitting
+    });
+  } // end submitTip()
 
 }
