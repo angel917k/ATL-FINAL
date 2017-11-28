02f9a02eb9001a1a91eaf41cb7609fad5028fb91
Jay <jaydiazzz10@gmail.com>
2017-11-27 20:58:54 -0800

    first push on jay branch

--- src/js/atl-js-code/splash.js
+++ src/js/atl-js-code/splash.js
@@ -3,6 +3,6 @@ $(document).ready(function(){
 new CircleType(document.getElementById('curve-text-1')).dir(-1).radius(115); //circular text deccoration
 
 $('.navbar-toggle').on('click', function() {
-  $('.after-navbar').css('margin-top','230px');  //scro ll do wn entire page on navbar click
+  $('.after-navbar').css('margin-top','230px');  //scroll do wn entire page on navbar click
 });
 });
