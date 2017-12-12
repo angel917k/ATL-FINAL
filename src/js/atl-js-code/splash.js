0f61884600000e8b59191bc846d9921d35c1ab46
angel917k <angel917k@gmail.com>
2017-12-11 18:42:42 -0800

    testing

--- src/js/atl-js-code/splash.js
+++ src/js/atl-js-code/splash.js
@@ -2,7 +2,7 @@
 $(document).ready(function(){
 
 $('.navbar-toggle').on('click', function() {
-  $('.after-navbar').css('margin-top','230px');  //scro ll do wn entire page on navbar click
+  $('.after-navbar').css('margin-top','230px');  //scr o  ll do wn entire page on navbar click
 });
 });
 console.log('boop')
